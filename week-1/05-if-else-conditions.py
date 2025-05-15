fc45c898fad8cf222d312d99a7f9fe7e29d2ef65
roni ebrar kevibeden <roni ebrar kevibeden@RONIEBRAR>
2025-05-15 03:10:37 +0200

    Fix: added content for if-else and loops

---
+++ week-1/05-if-else-conditions.py
@@ -0,0 +1,33 @@
+# Grade evaluation
+grade = 85
+
+if grade >= 90:
+    print("Grade: A")
+elif grade >= 80:
+    print("Grade: B")
+elif grade >= 70:
+    print("Grade: C")
+else:
+    print("Grade: F")
+
+
+# Age check
+age = 18
+
+if age >= 21:
+    print("You can drink alcohol in the USA")
+elif age >= 18:
+    print("You can vote")
+else:
+    print("You are a minor")
+
+
+# Number sign check
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Negative number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Positive number")
---
+++ week-1/06-loops-for-while.py
@@ -0,0 +1,24 @@
+# Using a for loop to iterate over a tuple
+fruits = ("apple", "banana", "cherry")
+for fruit in fruits:
+    print(fruit)
+
+# Print each letter in a string
+name = "Roni"
+for letter in name:
+    print(letter)
+
+# Print numbers using range
+for i in range(5):
+    print(i)
+
+# Colors example
+colors = ("red", "green", "blue")
+for color in colors:
+    print(color)
+
+# Using a while loop
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
